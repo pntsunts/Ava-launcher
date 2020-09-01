2520a4f394ccc4042de6acc7075a7055be11a844
Peter NTSUNTSHA <pntsunts@c4r11s11.wethinkcode.co.za>
2020-09-01 12:33:44 +0200

    push

--- FAils/Run.java
+++ FAils/Run.java
@@ -29,57 +29,49 @@ public class Run {
             Scanner some = new Scanner(myfile);
             List<String> ls = new ArrayList<>();
             try{
-            int i = 0;
+                int i = 0;
 
-            while (some.hasNextLine()) {
-                ls.add(some.nextLine());
-            }
-            some.close();
-            
-            if (ls.get(0) != null)
-            {
-                counter = Integer.parseInt(ls.get(0));
-
-                if (counter <= 0)
-                {
-                    System.out.println("Counter must be not be less or equal to 0");
-                    System.exit(1);
+                while (some.hasNextLine()) {
+                    ls.add(some.nextLine());
                 }
-            }
-            i = 1;
+                some.close();
             
-            while(i < ls.size()){      
-                
-                String lines[] = ls.get(i).split(" ");
-                if (lines.length == 5){
+                if (ls.get(0) != null) {
+                    counter = Integer.parseInt(ls.get(0));
 
-                   //Flyable fly = Aircraftfactory.newAircraft(lines[0], lines[1], Integer.parseInt(lines[2]), Integer.parseInt(lines[3]), Integer.parseInt(lines[4])); 
-                   try {
-                        AircraftFactory aircraftFactory = new AircraftFactory();
-                        aircraftFactory.newAircraft(lines[0], lines[1], Integer.parseInt(lines[2]), Integer.parseInt(lines[3]), Integer.parseInt(lines[4])).registerTower(weatherTower);
-                       
-                      } catch (Exception e) {
-                        System.out.println("HJBDJAJXHAXJAXajn");
-                        System.exit(0);
-                   }
-        
-                   
-                   /*if (fly != null)
-                   {
-                       fly.registerTower(weatherTower);
-                   }*/
+                    if (counter <= 0)
+                    {
+                        System.out.println("Counter must be not be less or equal to 0");
+                        System.exit(1);
+                    }
+                }
+                i = 1;
+            
+                while(i < ls.size()){      
+                    
+                    String lines[] = ls.get(i).split(" ");
+                    if (lines.length == 5){
 
+                    //Flyable fly = Aircraftfactory.newAircraft(lines[0], lines[1], Integer.parseInt(lines[2]), Integer.parseInt(lines[3]), Integer.parseInt(lines[4])); 
+                        try {
+                            AircraftFactory aircraftFactory = new AircraftFactory();
+                            aircraftFactory.newAircraft(lines[0], lines[1], Integer.parseInt(lines[2]), Integer.parseInt(lines[3]), Integer.parseInt(lines[4])).registerTower(weatherTower);
+                        } catch (Exception e) {
+                            System.out.println("HJBDJAJXHAXJAXajn");
+                            System.exit(0);
+                        }
                     }
                     else {
                         System.out.println("Error!! Format is TYPE NAME LONGITUDE LATITUDE HEIGHT ");
                         System.exit(1);
                     }
                     i++;
+                }
+            }
+            catch (Exception e){
+            System.out.println("SSSfsdv s");
             }
         }
-        catch (Exception e){
-        System.out.println("SSSfsdv s");
-        }}
         catch (FileNotFoundException e){
             System.out.println("ERROR FILE NOT FOUND");
             e.printStackTrace();
